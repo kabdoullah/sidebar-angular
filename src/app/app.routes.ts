4bb3f0deca146c427ce7cfa8134b03c1cb2bf7e8
Kemogoha Coulibaly <kemogoha.coulibaly@st2i.net>
2024-09-19 17:03:36 +0000

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProductsComponent } from './products/products.component';
+import { SettingsComponent } from './settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'settings', component: SettingsComponent },
+    { path: 'pages', component: PagesComponent },
+];
